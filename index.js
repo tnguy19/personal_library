0c77fec0dd15a77077d6dcfe275bffb5c9e44881
ttnguy19 <nguyenthinh19011@gmail.com>
2024-05-10 19:17:51 -0400

    website can display books sorted by rating or by date read

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://covers.openlibrary.org/b/isbn/";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,9 +20,41 @@ const db = new pg.Client({
 
 db.connect();
 
+let books =[];
+let sortingCriteria = "date_read DESC";
+
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
+  try {
+    const result = await db.query("SELECT * FROM books ORDER BY " + sortingCriteria);
+    books = result.rows;
+    console.log(result.rows);
+    //console.log(books);
+    for (let item of books) {
+      try {
+        const result = API_URL + item.isbn + "-M.jpg";
+        item.image = result;
+        //await db.query("INSERT INTO cover_images (book_id, image) VALUES ($1, $2)", [item.id, book_cover]);
+      } catch (err) {
+        console.log(`Error fetching book cover for ISBN ${item.isbn}:`, err);
+      }
+    }
+    res.render("index.ejs", {
+      books: books
+    });
+  } catch (err) {
+    console.log(err);
+  }
   });   
+
+app.get("/date", async (req,res) => {
+  sortingCriteria = "date_read DESC";
+  res.redirect("/");
+  })
+
+app.get("/rating", async (req,res) => {
+    sortingCriteria = "rating DESC";
+    res.redirect("/");
+  })
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- queries.sql
+++ queries.sql
@@ -7,3 +7,16 @@ CREATE TABLE books (
     comment TEXT,
     link VARCHAR(255) NOT NULL
 );
+
+INSERT INTO books (title, isbn, date_read, rating, comment, link)
+VALUES ('Harry Potter and the Philosophers'' Stone','9781408855652', '2018-10-01, 10', 'Nice book', 'https://a.co/d/9CR24K6');
+
+CREATE TABLE cover_images (
+	id SERIAL PRIMARY KEY,
+	book_id INTEGER REFERENCES books(id),
+	image bytea
+);
+
+SELECT *
+FROM books JOIN cover_images
+ON books.id = cover_images.book_id;
