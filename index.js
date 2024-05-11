fc6ea731f3809c175e165425736f940eebf3b7da
ttnguy19 <nguyenthinh19011@gmail.com>
2024-05-11 19:14:59 -0400

    user can now edit post

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "personal_library",
-  password: "", //Remove when upload
+  password: "Culun2003", //Remove when upload
   port: 5432,
 });
 
@@ -27,13 +27,11 @@ app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM books ORDER BY " + sortingCriteria);
     books = result.rows;
-    console.log(result.rows);
+    //console.log(result.rows);
     //console.log(books);
     for (let item of books) {
       try {
-        const result = API_URL + item.isbn + "-M.jpg";
-        item.image = result;
-        //await db.query("INSERT INTO cover_images (book_id, image) VALUES ($1, $2)", [item.id, book_cover]);
+        item.image = findCoverURL(item.isbn);
       } catch (err) {
         console.log(`Error fetching book cover for ISBN ${item.isbn}:`, err);
       }
@@ -56,7 +54,36 @@ app.get("/rating", async (req,res) => {
     res.redirect("/");
   })
 
+app.get("/edit/:id", async (req,res) => {
+  try {
+    const result = await db.query("SELECT * FROM books WHERE id = $1", [req.params.id]);
+    let book = result.rows[0];
+    book.image = findCoverURL(book.isbn);
+    //console.log(book);
+    res.render("edit.ejs", {
+      book: book
+    });
+  } catch (err) {
+    console.log(`Error fetching book for edit with id ${req.params.id}:`, err);
+  }
+})
+
+app.post("/edit/:id", async (req,res) => {
+  try {
+    //console.log(req.body);
+    const editPost = req.body;
+    await db.query("UPDATE books SET title = $1, isbn = $2, date_read = $3, rating = $4,comment = $5, link = $6 WHERE id = $7", [editPost.title, editPost.isbn, editPost.date, editPost.rating, editPost.comment, editPost.link, editPost.id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(`Error saving edit for book with id ${req.params.id}:`, err);
+  }
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
     //console.log(posts);
   });
+
+  function findCoverURL(isbn){
+    return API_URL + isbn + "-M.jpg";
+  }
